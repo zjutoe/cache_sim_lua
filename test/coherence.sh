e6ea3bf23d36eac5bac06ddd9516469a0423ed6f
Toe Zju <zjutoe@gmail.com>
2015-03-01 22:13:34 +0800

    test coherence.lua with date.mref.log

--- test/coherence.sh
+++ test/coherence.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-luajit test_coherence.lua test_coherence.trace
+
+if [ ! -h cache.lua ]; then
+    ln -s ../cache.lua
+fi
+
+#luajit test_coherence.lua test_coherence.trace
+luajit test_coherence.lua date.mref.log
+
